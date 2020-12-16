f4e9c9ac6e16a9f1d7114d056b24193c0c87ea72
Mher Davtyan <mher.davtyan@Mhers-MacBook-Pro.local>
2020-12-16 15:40:13 +0400

    Detect floor

--- BodyDetection/ViewController.swift
+++ BodyDetection/ViewController.swift
@@ -10,68 +10,125 @@ import RealityKit
 import ARKit
 import Combine
 
-class ViewController: UIViewController, ARSessionDelegate {
+class ViewController: UIViewController {
 
-    @IBOutlet var arView: ARView!
+    // MARK: - Outlets
     
-    // The 3D character to display.
+    @IBOutlet var arView: ARView!
+    @IBOutlet weak var warningView: UIView!
+    @IBOutlet weak var warningLabel: UILabel!
+    
+    // MARK: - Properties
+    
     var character: BodyTrackedEntity?
-    let characterOffset: SIMD3<Float> = [-1.0, 0, 0] // Offset the character by one meter to the left
     let characterAnchor = AnchorEntity()
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
+    // MARK: - Lifecycle
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        showWarning(with: "Click on the floor")
         arView.session.delegate = self
         
-        // If the iOS device doesn't support body tracking, raise a developer error for
-        // this unhandled case.
         guard ARBodyTrackingConfiguration.isSupported else {
             fatalError("This feature is only supported on devices with an A12 chip")
         }
 
-        // Run a body tracking configration.
         let configuration = ARBodyTrackingConfiguration()
+        configuration.planeDetection = .horizontal
         arView.session.run(configuration)
-        
         arView.scene.addAnchor(characterAnchor)
         
-        // Asynchronously load the 3D character.
+        loadCharacter()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        arView.session.pause()
+    }
+    
+    // MARK: - Methods
+    
+    func loadCharacter() {
         var cancellable: AnyCancellable? = nil
-        cancellable = Entity.loadBodyTrackedAsync(named: "character/robot").sink(
-            receiveCompletion: { completion in
-                if case let .failure(error) = completion {
-                    print("Error: Unable to load model: \(error.localizedDescription)")
+        cancellable = Entity
+            .loadBodyTrackedAsync(named: "character/robot.usdz")
+            .sink(
+                receiveCompletion: { completion in
+                    if case let .failure(error) = completion {
+                        print("Error: Unable to load model: \(error.localizedDescription)")
+                    }
+                    cancellable?.cancel()
+                },
+                receiveValue: { (character: Entity) in
+                    if let character = character as? BodyTrackedEntity {
+                        // Scale the character to human size
+                        character.scale = [1.0, 1.0, 1.0]
+                        self.character = character
+                        cancellable?.cancel()
+                    } else {
+                        print("Error: Unable to load model as BodyTrackedEntity")
+                    }
                 }
-                cancellable?.cancel()
-        }, receiveValue: { (character: Entity) in
-            if let character = character as? BodyTrackedEntity {
-                // Scale the character to human size
-                character.scale = [1.0, 1.0, 1.0]
-                self.character = character
-                cancellable?.cancel()
-            } else {
-                print("Error: Unable to load model as BodyTrackedEntity")
+            )
+    }
+    
+    func detectFloor(_ point: CGPoint) {
+        let results = arView.hitTest(point, types: [.existingPlaneUsingExtent])
+        let planeAnchors = results.filter({
+            guard let anchor = $0.anchor as? ARPlaneAnchor else { return false }
+            switch anchor.classification {
+            case .floor:
+                return true
+            default:
+                return false
             }
         })
+        
+        guard let position = planeAnchors.first?.worldTransform.columns.3 else {
+            showWarning(with: "Floor not found")
+            return
+        }
+        hideWarning()
+        
+        // Set new character position
+        characterAnchor.position = simd_make_float3(position)
+        characterAnchor.position.y += character?.scale.y ?? 1 / 2
+        
+        // Add the character with updated transformation and position
+        if let character = character, character.parent == nil {
+            characterAnchor.addChild(character)
+        }
     }
+    
+    // MARK: - UI Methods
+    
+    func showWarning(with text: String) {
+        warningLabel.text = text
+        warningView.isHidden = false
+    }
+    
+    func hideWarning() {
+        warningView.isHidden = true
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func arViewTapped(_ sender: UITapGestureRecognizer) {
+        detectFloor(sender.location(in: arView))
+    }
+}
+
+// MARK: - ARSessionDelegate
+
+extension ViewController: ARSessionDelegate {
     
     func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
         for anchor in anchors {
             guard let bodyAnchor = anchor as? ARBodyAnchor else { continue }
             
-            // Update the position of the character anchor's position.
-            let bodyPosition = simd_make_float3(bodyAnchor.transform.columns.3)
-            characterAnchor.position = bodyPosition + characterOffset
-            // Also copy over the rotation of the body anchor, because the skeleton's pose
-            // in the world is relative to the body anchor's rotation.
+            // Assign the human body transformation to the charachter
             characterAnchor.orientation = Transform(matrix: bodyAnchor.transform).rotation
-   
-            if let character = character, character.parent == nil {
-                // Attach the character to its anchor as soon as
-                // 1. the body anchor was detected and
-                // 2. the character was loaded.
-                characterAnchor.addChild(character)
-            }
         }
     }
 }
